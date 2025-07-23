9520d249d3bf4099ba8ffcb1bc2585bc1c539e01
さゆき <161200388+SayukiDev@users.noreply.github.com>
2025-07-23 15:49:50 +0900

    fix: close voicevox when switch to remote mode

--- internal/service/vv.go
+++ internal/service/vv.go
@@ -11,6 +11,7 @@ func (s *Service) initVoiceVox(opt *options.Options) error {
 	s.VV.SetSpeaker(opt.VoiceVox.Selected)
 	opt.AddHook(func(o *options.Options) error {
 		if o.VoiceVox.IsRemote {
+			s.VV.Close()
 			s.VV = voicevox.New(opt.VoiceVox.Path, opt.VoiceVox.LineLimit, &opt.VoiceVox)
 		}
 		s.VV.SetSpeaker(opt.VoiceVox.Selected)
